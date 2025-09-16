07ae46244887db2948bfc81d69a36f2374223913
Nadia <1abdallah.nadia@gmail.com>
2025-09-16 23:54:04 +0200

    feat:create staging models

---
+++ creche_project/models/staging/stg__agrements_assistants_mater_reg.sql
@@ -0,0 +1,4 @@
+{{ config(materialized='view') }}
+
+select *
+from {{ ref('agrements_assistants_mater_dep') }}
---
+++ creche_project/models/staging/stg__places_creches_collectives_dep.sql
@@ -0,0 +1,4 @@
+{{ config(materialized='view') }}
+
+select *
+from {{ ref('places_creches_collectives_dep') }}
